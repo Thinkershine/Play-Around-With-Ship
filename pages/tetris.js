e2e22d6ca3d47318b8ab30dadff49b6096f95da3
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-08-12 06:31:47 +0200

    WORK IN PROGRESS - Between Places Transport

--- pages/tetris.js
+++ pages/tetris.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useReducer } from "react";
 
 const isDev = false;
 const isPlaying = false;
+const blocksFallingSpeed = 4000;
 
 const blockTypes = {
   square: [
@@ -35,14 +36,6 @@ const blockTypes = {
     { color: "BLUE", row: 1, col: 1 }
   ]
 };
-
-const emptyBoard = [
-  ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
-  ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
-  ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
-  ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
-  ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"]
-];
 
 const colouredBoard = [
   [
@@ -96,114 +89,167 @@ const initialBoard = [
   ]
 ];
 
-const boardReducer = (board, action) => {
-  console.log("STATE AT REDUCER", board[0][2]);
-  console.log("PAYLOAD TO SET", action.payload.currentBlock[0].color);
-
-  console.log("STATE", board);
-  //   let newBoard = state.map(block => block);
-  //   newBoard[0][2] = action.payload.currentBlock[0][0];
-  //   console.log("NEW BOARD", newBoard);
-  //   (state[0][2] = action.payload.currentBlock[0][0]);
+const boardReducer = (state, action) => {
+  console.log("BOARD STATE", state.gameBoard);
 
   switch (action.type) {
     case "NEW_BLOCK":
+      console.log("CURRENT BLOCK", action.payload.currentBlock);
       const currentBlock = action.payload.currentBlock;
+      state.currentBlock = currentBlock;
+
       const initialRow = 0;
       const initialCol = 2;
       let row = initialRow;
       let col = initialCol;
 
+      // Update GameBoard
       for (let i = 0; i < currentBlock.length; i += 1) {
         if (i === 0) {
-          board[row][col] = {
+          state.gameBoard[row][col] = {
             color: currentBlock[0].color,
-            index: board[row][col].index
+            index: state.gameBoard[row][col].index
           };
-
-          // update block
         } else {
           row = initialRow + currentBlock[i].row;
           col = initialCol + currentBlock[i].col;
-          console.log("BLOCK ROW ", row, "BLOCK COL ", col);
-          board[row][col] = {
+          state.gameBoard[row][col] = {
             color: currentBlock[i].color,
-            index: board[row][col].index
+            index: state.gameBoard[row][col].index
           };
         }
+
+        // Update CurrentBlock
+        state.currentBlock[i] = {
+          color: currentBlock[0].color,
+          row: row,
+          col: col
+        };
+
+        console.log("BLOCK ROW ", row, "BLOCK COL ", col);
       }
-      //   console.log("SPREADED STATE ", ...state);
-      return board;
+      return { ...state, currentBlock: state.currentBlock };
+    case "MOVE_BLOCKS":
+      const firstRow = 0;
+      const lastRow = state.gameBoard.length;
+      // update blocks positions
+      for (let row = 0; row < state.gameBoard.length; row += 1) {
+        for (let col = 0; col < state.gameBoard[0].length; col += 1) {
+          if (state.gameBoard[row][col].color !== "EMPTY" && row === 0) {
+            state.gameBoard[row][col].color = "EMPTY";
+          }
+          if (row !== firstRow) {
+            if (state.gameBoard[row - 1][col].color !== "EMPTY") {
+              // If above this space is block visible >>> FALL IT DOWN
+
+              state.gameBoard[row][col].color =
+                state.gameBoard[row - 1][col].color;
+              state.gameBoard[row - 1][col].color = "EMPTY";
+
+              // check if below is free spot!
+              // ++ CHECK ALL BLOCK IF IT's NOT TOUCHING ANYTHING
+            }
+          }
+        }
+      }
+
+      return state;
+    case "MOVE_BLOCK_DOWN":
+      // Update GameBoard
+      // Update CurrentBlock
+      //// Check for Collisions
+      ////// Check for Left & Right Collision
+      ////// Check for Last Row
+      console.log("CURRENT BLOCK AT MOVE DOWN", state.currentBlock);
+
+      const updatedBlock = state.currentBlock;
+      for (let i = 0; i < updatedBlock.length; i += 1) {
+        if (updatedBlock[i].row !== state.gameBoard.length) {
+          // Check if Block Under is Empty
+          if (state.gameBoard[i + 1][updatedBlock[i].col].color === "EMPTY") {
+            updatedBlock[i].row += 1;
+          } else {
+            console.log(
+              "BLOCK UNDER IS TAKEN",
+              state.gameBoard[i + 1][updatedBlock[i].col]
+            );
+          }
+        } else {
+          console.log(
+            "BLOCK HITTED BOTTOM",
+            updatedBlock[i].row,
+            "BOARD BOTTOM ROW",
+            state.gameBoard.length
+          );
+        }
+
+        // UPDATE GameBoard
+        for (let i = 0; i < updatedBlock.length; i += 1) {
+          let updatedBlockRow = updatedBlock[i].row;
+          let updatedBlockCol = updatedBlock[i].col;
+
+          state.gameBoard[updatedBlockRow][updatedBlockCol] = {
+            color: updatedBlock[i].color,
+            index: state.gameBoard[updatedBlockRow][updatedBlockCol].index
+          };
+
+          // clear old blocks
+          if (i !== 0) {
+            state.gameBoard[updatedBlockRow - 1][updatedBlockCol] = {
+              color: "EMPTY",
+              index: state.gameBoard[updatedBlockRow][updatedBlockCol].index
+            };
+          }
+        }
+      }
+      console.log("UPDATED CURRENT BLOCK ", updatedBlock);
+      return { ...state, currentBlock: updatedBlock };
     default:
-      return board;
+      return state;
   }
 };
 
 const Tetris = () => {
-  const startingBoard = isDev
-    ? initialBoard
-    : isPlaying
-    ? emptyBoard
-    : colouredBoard;
+  const [gameState, setGameState] = useState({
+    currentBlock: [],
+    newBlockRendered: false
+  });
+
   const [drawnBoard, setDrawnBoard] = useState([]);
-  const [board, dispatchBoard] = useReducer(boardReducer, initialBoard); // change to programmatic board
+  const [state, dispatchBoard] = useReducer(boardReducer, {
+    gameBoard: initialBoard,
+    currentBlock: []
+  });
   const [nextBlock, setNextBlock] = useState(blockTypes.pBlock);
+
   useEffect(() => {
-    let playTimer = setInterval(moveBlocks, 5000);
+    let playTimer = setInterval(moveBlocks, blocksFallingSpeed);
     return () => {
       clearInterval(playTimer);
     };
   });
 
   const moveBlocks = () => {
-    console.log("BLOCKS MOVING NEW BOARD >", board);
+    if (!gameState.newBlockRendered) {
+      newBlock();
+    } else {
+      dispatchBoard({
+        type: "MOVE_BLOCK_DOWN"
+      });
+    }
+    drawBoard();
+  };
+
+  const newBlock = () => {
     dispatchBoard({
       type: "NEW_BLOCK",
       payload: { currentBlock: nextBlock }
     });
-    drawBoard();
-  };
 
-  const drawBlock = block => {
-    // console.log("DRAW BLOCK COLOR", block.color);
-    let blockColor = { backgroundColor: "red" };
-    switch (block.color) {
-      case "GREEN":
-        blockColor = { backgroundColor: "green" };
-        break;
-      case "RED":
-        blockColor = { backgroundColor: "red" };
-        break;
-      case "YELLOW":
-        blockColor = { backgroundColor: "yellow" };
-        break;
-      case "BLUE":
-        blockColor = { backgroundColor: "blue" };
-        break;
-      case "BLACK":
-        blockColor = { backgroundColor: "black" };
-        break;
-      default:
-        blockColor = { backgroundColor: "transparent" };
-        break;
-    }
-
-    // console.log("AFTER COLOURING", blockColor);
-    return (
-      <div key={block.index} className="block" style={blockColor}>
-        <p
-          style={{
-            color: "black",
-            height: 25,
-            width: 25,
-            margin: 0,
-            padding: 0
-          }}
-        >
-          {isDev ? blockColor : ""}
-        </p>
-      </div>
-    );
+    setGameState({
+      currentBlock: nextBlock,
+      newBlockRendered: true
+    });
   };
 
   const boardStyles = {
@@ -237,15 +283,15 @@ const Tetris = () => {
   const drawBoard = () => {
     const redrawnBoard = [];
 
-    for (let i = 0; i < board.length; i += 1) {
+    for (let i = 0; i < state.gameBoard.length; i += 1) {
       let columns = [];
 
-      for (let j = 0; j < board[i].length; j += 1) {
-        console.log("DRAW COL ", j, " ROW ", i);
+      for (let j = 0; j < state.gameBoard[i].length; j += 1) {
+        // console.log("DRAW COL ", j, " ROW ", i);
         columns.push(
           <div key={"col" + j} className="col" style={boardStyles.colStyle}>
-            {drawBlock(board[i][j])}
-            {console.log("BOARD PIECE", board[i][j])}
+            {drawBlock(state.gameBoard[i][j])}
+            {/* {console.log("BOARD PIECE", board[i][j])} */}
           </div>
         );
       }
@@ -257,6 +303,46 @@ const Tetris = () => {
     }
 
     setDrawnBoard(redrawnBoard);
+  };
+
+  const drawBlock = block => {
+    let blockColor = { backgroundColor: "red" };
+    switch (block.color) {
+      case "GREEN":
+        blockColor = { backgroundColor: "green" };
+        break;
+      case "RED":
+        blockColor = { backgroundColor: "red" };
+        break;
+      case "YELLOW":
+        blockColor = { backgroundColor: "yellow" };
+        break;
+      case "BLUE":
+        blockColor = { backgroundColor: "blue" };
+        break;
+      case "BLACK":
+        blockColor = { backgroundColor: "black" };
+        break;
+      default:
+        blockColor = { backgroundColor: "transparent" };
+        break;
+    }
+
+    return (
+      <div key={block.index} className="block" style={blockColor}>
+        <p
+          style={{
+            color: "black",
+            height: 25,
+            width: 25,
+            margin: 0,
+            padding: 0
+          }}
+        >
+          {isDev ? blockColor : ""}
+        </p>
+      </div>
+    );
   };
 
   return (
