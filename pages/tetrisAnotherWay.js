efb225a9b156ddf1d9391a8b2e0061526e126eb3
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-08-15 19:20:57 +0200

    Draw First Piece

--- pages/tetrisAnotherWay.js
+++ pages/tetrisAnotherWay.js
@@ -3,12 +3,28 @@ import React, { useRef, useEffect } from "react";
 const TetrisAnother = () => {
   const board = useRef(null);
 
+  const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
+
   useEffect(() => {
     const canvas = board.current;
     const context = canvas.getContext("2d");
+    context.scale(20, 20);
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawMatrix(matrix, { x: 5, y: 5 }, context);
   });
+
+  const drawMatrix = (matrix, offset, context) => {
+    matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          context.fillStyle = "red";
+          context.fillRect(x + offset.x, y + offset.y, 1, 1);
+        }
+      });
+    });
+  };
 
   return (
     <div>
