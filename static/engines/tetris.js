3e10dd648a040d39ea626ea426a3fd7e53c99a3c
Thinkershine <siemaszkosebastian@gmail.com>
2019-08-21 16:32:51 +0200

    Add Rotation

--- static/engines/tetris.js
+++ static/engines/tetris.js
@@ -75,6 +75,34 @@ function playerMove(dir) {
   }
 }
 
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; y += 1) {
+    for (let x = 0; x < y; x += 1) {
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+    }
+  }
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -114,7 +142,11 @@ const handleKeyPress = event => {
     case 40:
       playerDrop();
       break;
-    case 68:
+    case 81:
+      playerRotate(-1);
+      break;
+    case 87:
+      playerRotate(1);
       break;
     default:
       console.log("Other Key", event.key, "Key Code", event.keyCode);
