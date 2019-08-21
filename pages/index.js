6429d1e025fd480c8ded463c46fed697976d2037
Thinkershine <siemaszkosebastian@gmail.com>
2019-08-21 17:07:36 +0200

    Add Some Styling

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,26 @@ const Index = () => {
     <div>
       <Head>
         <title>Tetris</title>
+        <style jsx-global>
+          {`
+            body {
+              background: #202028;
+              color: #fff;
+              font-family: sans-serif;
+              font-size: 2em;
+              text-align: center;
+            }
+            canvas {
+              border: 1px solid #fff;
+            }
+          `}
+        </style>
       </Head>
       <h1>Tetris With Engine</h1>
       <div>
         <canvas id="tetris" width="240" height="400" />
       </div>
-      <p id="score" />
+      <p id="score">0</p>
       <script src="/static/engines/tetris.js" />
     </div>
   );
