0d86216d947d444c31441276ad170a66c29dd730
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-08-21 08:09:52 +0200

    Cleaning...

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import Head from "next/head";
 const Index = () => {
   return (
     <div>
-      <Head />
+      <Head>
+        <title>Tetris</title>
+      </Head>
       <h1>Tetris With Engine</h1>
       <div>
-        {" "}
         <canvas id="tetris" width="240" height="400" />
       </div>
-      <p id="msg" />
       <script src="/static/engines/tetris.js" />
     </div>
   );
--- static/engines/tetris.js
+++ static/engines/tetris.js
@@ -4,24 +4,15 @@ const context = canvas.getContext("2d");
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-const yoMe = () => {
-  alert("YO");
-};
-
 const handleKeyPress = event => {
-  console.log("EVENT", event);
   switch (event.keyCode) {
     case 37:
-      alert("YO 37");
       break;
     case 38:
-      alert("YO 38");
       break;
     case 39:
-      alert("YO 39");
       break;
     case 40:
-      alert("YO 40");
       break;
     case 68:
       break;
